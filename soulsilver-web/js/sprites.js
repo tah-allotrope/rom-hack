56334a6d79bc16ec59caa39f36db4560b65de2a1
tah-allotrope <tah@allotropevc.com>
2026-09-11 23:39:48 +0700

    Gauntlet: double-width fangs

--- soulsilver-web/js/sprites.js
+++ soulsilver-web/js/sprites.js
@@ -323,7 +323,7 @@ const F_AQUAPUP = [
   "....CBC...CCCCEEEEECCCC..BDB....",
   ".....C....BCCKKKKKKECCB...B.....",
   "...........BCWKKKKWECB..........",
-  "...........CCWKKKKWCCC..........",
+  "...........CCWWKKKKWWCC.........",
   "..........CCCKKKKKKECCC.........",
   "..........BCCKMMMMKECCB.........",
   ".........BBCCKMMMMKECCBB........",
